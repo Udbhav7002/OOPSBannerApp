3b321fd08342da63c0a0c3944a6eb3e968db51cc
Udbhav <sanagariudbhav@gmail.com>
2026-02-17 10:55:15 +0530

    Printed OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -8,18 +8,21 @@
  * @version 3.0
  */
 
-public class OOPSBannerApp {
-
+public class OOPSBanner {
     public static void main(String[] args) {
-
-        // Each line using String.join()
-        System.out.println(String.join(" ", " ***** ", " ***** ", " ******", " ******"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     "));
-        System.out.println(String.join(" ", "*     *", "*     *", "****** ", " *****"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "     *"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "*    *"));
-        System.out.println(String.join(" ", " ***** ", " ***** ", "*      ", " ****"));
-
+        // Line 1
+        System.out.println("  *** " + "  *** " + " ***** " + "  ***** ");
+        // Line 2
+        System.out.println(" ** ** " + " ** ** " + " ** ** " + " ** ");
+        // Line 3
+        System.out.println("** ** " + "** ** " + " ** ** " + " ** ");
+        // Line 4
+        System.out.println("** ** " + "** ** " + " ***** " + "  ***** ");
+        // Line 5
+        System.out.println("** ** " + "** ** " + " ** " + "      **");
+        // Line 6
+        System.out.println(" ** ** " + " ** ** " + " ** " + "      **");
+        // Line 7
+        System.out.println("  *** " + "  *** " + " ** " + "  ***** ");
     }
 }
